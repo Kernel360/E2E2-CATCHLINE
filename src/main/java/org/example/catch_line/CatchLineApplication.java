d2afe187dde298aa9103214a82a3bb39a9532f91
sangwonsheep <swonyy1709@naver.com>
2024-07-30 17:37:45 +0900

    project setting

---
+++ src/main/java/org/example/catch_line/CatchLineApplication.java
@@ -0,0 +1,13 @@
+package org.example.catch_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/catch_line/CatchLineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.catch_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

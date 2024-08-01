a9d88efdd2fc2d9569bc9804c2f5b5d66a855be3
I-migi <sunn10189@gmail.com>
2024-08-01 21:12:09 +0900

    Waiting Test 구현

--- src/main/java/org/example/catch_line/waiting/model/dto/WaitingRequest.java
+++ src/main/java/org/example/catch_line/waiting/model/dto/WaitingRequest.java
@@ -19,22 +19,11 @@ import lombok.Setter;
 @AllArgsConstructor
 public class WaitingRequest {
 
-<<<<<<< HEAD
-=======
-	@NotBlank(message = "waitingId 는 채워져있어야 합니다!")
-	private Long waitingId;
 
->>>>>>> 3c60282f0c8264a62e98221854c3026b2536d8ca
 	@NotBlank
 	@Min(value = 1, message = "최소 인원 수는 1명입니다")
 	private int memberCount;
 
-<<<<<<< HEAD
-=======
-	@NotBlank(message = "상태는 빈 값일 수 없습니다.")
-	private WaitingStatus waitingStatus;
-
->>>>>>> 3c60282f0c8264a62e98221854c3026b2536d8ca
 	@NotBlank(message = "포장이나 매장을 선택해야 합니다")
 	private WaitingType waitingType;
 }

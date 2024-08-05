5c5cd916626be34a743809b23cb2ec4b6845f14d
소은 <soeun@so-eun-ui-MacBookAir.local>
2024-08-05 15:40:08 +0900

    feat: 사용자가 스크랩한 식당 조회

--- src/main/java/org/example/catch_line/member/service/MyScrapService.java
+++ src/main/java/org/example/catch_line/member/service/MyScrapService.java
@@ -1,4 +1,30 @@
 package org.example.catch_line.member.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.catch_line.member.model.entity.MemberEntity;
+import org.example.catch_line.member.validate.MemberValidator;
+import org.example.catch_line.restaurant.model.dto.RestaurantResponse;
+import org.example.catch_line.restaurant.model.mapper.RestaurantMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class MyScrapService {
+
+    private final MemberValidator memberValidator;
+    private final RestaurantMapper restaurantMapper;
+
+    public List<RestaurantResponse> findMyRestaurants(Long memberId) {
+
+        MemberEntity member = memberValidator.checkIfMemberPresent(memberId);
+
+        return member.getRestaurantScraps().stream()
+                .map(restaurantMapper::toDto)
+                .collect(Collectors.toList());
+
+    }
+
 }

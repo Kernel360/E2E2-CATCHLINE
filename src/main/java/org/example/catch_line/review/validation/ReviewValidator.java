b043c2c11f285c03656634f15e7d40d8550229db
sangwonsheep <swonyy1709@naver.com>
2024-08-11 01:27:56 +0900

    리뷰 서비스 테스트 구현, 로직 수정

---
+++ src/main/java/org/example/catch_line/review/validation/ReviewValidator.java
@@ -0,0 +1,18 @@
+package org.example.catch_line.review.validation;
+
+import lombok.RequiredArgsConstructor;
+import org.example.catch_line.review.model.entity.ReviewEntity;
+import org.example.catch_line.review.repository.ReviewRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewValidator {
+
+    private final ReviewRepository reviewRepository;
+
+    public ReviewEntity checkIfReviewPresent(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 식당이 없습니다."));
+    }
+}

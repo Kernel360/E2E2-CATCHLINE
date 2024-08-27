a4e9bfb34f8c6a20a0006272e930504730d22779
I-migi <sunnn10189@gmail.com>
2024-08-27 11:13:36 +0900

    request NoArgsConstructor 제거

--- src/main/java/org/example/catch_line/user/member/model/dto/LoginRequest.java
+++ src/main/java/org/example/catch_line/user/member/model/dto/LoginRequest.java
@@ -3,7 +3,6 @@ package org.example.catch_line.user.member.model.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-// TODO: No, Setter 타임리프에서 왜  붙여야 하는지? -> 뺐음.
 @AllArgsConstructor
 @Getter
 @Builder

d7bb60d75cf9495bfcb1c0af61ebb55d5828cab0
soeunnPark <carol0724@naver.com>
2024-08-28 17:16:17 +0900

    불필요한 import 문 제거

--- src/main/java/org/example/catch_line/user/owner/controller/OwnerAuthController.java
+++ src/main/java/org/example/catch_line/user/owner/controller/OwnerAuthController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import static org.example.catch_line.common.session.SessionConst.*;
 
 @Controller
 @RequestMapping("/owner")
